c0ca69ab6b25922472e900cf60657af76f75b439
nikitarub <nikita.rub@yandex.ru>
2018-05-13 20:25:02 +0300

    4 Add ui

--- sem_4/oop/lab_04/mainwindow.h
+++ sem_4/oop/lab_04/mainwindow.h
@@ -14,6 +14,14 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    bool ventState = false;
+
+private slots:
+    void on_ventButton_clicked();
+
+    void on_closeDoorsInLift_clicked();
+
+    void on_openDoorsInLift_clicked();
 
 private:
     Ui::MainWindow *ui;
